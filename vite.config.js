2c8b5caacbf77a22b59bdf78bfaf81fbdf61439f
Elahe <elasdmomen@gmail.com>
2025-11-28 10:17:36 +0330

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   base: '/dastres/',
 })
